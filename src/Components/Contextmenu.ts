8304e94f58c8e57f47cebaa2388b23cae1633cd1
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-05-26 19:57:33 +0200

    change: Contextmenu

--- src/Components/Contextmenu.ts
+++ src/Components/Contextmenu.ts
@@ -43,7 +43,8 @@ export class ContextMenu extends Event {
     }
 
     private addListener(): void {
-        this.props.items[0]?.btn.el.focus({ focusVisible: true });
+        // this.props.items[0]?.btn.el.focus({ focusVisible: true }); Wird nur in Firefox unterstützt ist noch relativ neu
+        this.props.items[0]?.btn.el.focus();
         this.props.items.forEach(item => {
             let handler = typeof item.event === 'function' ? item.event : () =>  console.log('Kein Event');
             item.btn.onClick(() => {
